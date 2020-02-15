a27bb2f661c9c823d1a4b6af6b33ec4760dcc732
Daryl Alejandro Olivares Gutierrez <daryl_alejandro@hotmail.com>
2020-02-15 17:29:23 -0300

    Basics

---
+++ index.js
@@ -0,0 +1,51 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+const ytdl = require('ytdl-core');
+
+const token = 'Njc0ODU3MzUwNzc5MzcxNTUx.XkesFQ.Q9OmszA4c1MoBYtY-28-UhsU6vQ';
+
+const PREFIX = '';
+
+var servers = {};
+
+bot.on('ready', () =>{
+    console.log('Bot online');
+})
+
+bot.on('message', message =>{
+    
+    let args = message.content.substring(PREFIX.lenght).split("");
+
+    var msg = message.content.toLowerCase();
+
+    if (message.author.bot) return;
+
+    if(message.channel.id == "514929154555117608"){
+
+        if(!message.member.voiceChannel){
+            return;
+        }
+
+        if(!message.guild.voiceConnection) message.member.voiceChannel.join().then(function(connection){
+            var url = 'https://www.youtube.com/watch?v=u9o0DkInNls';
+            connection.playStream(ytdl(url, {filter: "audioonly"}));
+
+            setTimeout(timeout, 18000);
+            function timeout(){
+                connection.disconnect()
+            }
+
+        })
+    }
+
+    if (msg.includes("uwu")){
+        message.channel.sendMessage('OwO');
+    }
+
+    if (msg.includes("owo")){
+        message.channel.sendMessage('UwU');
+    }
+
+})
+
+bot.login(token);
