34a9b8a3b2a44c834104791ac4d5a08f19bb3fa0
Daryl Alejandro Olivares Gutierrez <daryl_alejandro@hotmail.com>
2020-02-18 04:12:58 -0300

    aaaaaaaaaaa

--- index.js
+++ index.js
@@ -197,7 +197,7 @@ bot.on('message', message =>{
 
                 var string = JSON.stringify(body);
                 var nombres = JSON.parse(string);
-                console.log(nombres['name']);
+                console.log(nombres);
             })
         }).end();
 
