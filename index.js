6e2b244a7b281ade9fed96c72fea4f23f527c048
Daryl Alejandro Olivares Gutierrez <daryl_alejandro@hotmail.com>
2020-02-18 02:32:52 -0300

    3

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 const ytdl = require('ytdl-core');
-var steamAPI = require('apis/steamapi.js');
+//var steamAPI = require('apis/steamapi.js');
 
 const token = process.env.TOKEN;
 
@@ -174,7 +174,8 @@ bot.on('message', message =>{
 
 
     if (msg == "."){
-        steamAPI.getGames(steamKey);
+        var games = request.get('http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=F987C03CC424ECB0FD537F650B75A52B&steamid=76561198021608065&format=xml&include_appinfo=true');
+        message.channel.sendMessage(games);
     }
 
 })
