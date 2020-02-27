a722a94f9362f0f82fc59e59d0624128c6fe9a23
Daryl Alejandro Olivares Gutierrez <daryl_alejandro@hotmail.com>
2020-02-27 03:31:41 -0300

    Fix del fix del fix

--- index.js
+++ index.js
@@ -111,7 +111,7 @@ bot.on('message', message =>{
             var current_date = new Date();
             var alarma_date = new Date();
             
-            current_date.setHours(current_date.getHours()+3, current_date.getMinutes());
+            current_date.setHours(current_date.getHours()-3, current_date.getMinutes());
 
             var fecha_alarma = args[1];
             var hora_alarma = args[2];
@@ -122,7 +122,7 @@ bot.on('message', message =>{
             var mes = split_fecha[1]-1;
             var ano = split_fecha[2];
             alarma_date.setFullYear(ano,mes,dia);
-            alarma_date.setHours(split_hours[0]-3,split_hours[1]);
+            alarma_date.setHours(split_hours[0],split_hours[1]);
             if(current_date < alarma_date){
                 var mils_current = current_date.getTime();
                 var mils_alarma = alarma_date.getTime();
@@ -149,8 +149,8 @@ bot.on('message', message =>{
                 message.channel.send("Acaso quieres viajar al pasado? Crees que esta wea es steins;gate? >:C");
                 console.log('Hora actual: '+current_date+ 'hora programada: '+alarma_date);
             }
-            
-            console.log('Hora actual: '+current_date+ 'hora programada: '+alarma_date);
+            console.log('Hora actual: '+current_date+ '  hora programada: '+alarma_date);
+
         }else{
             message.channel.send("Faltaron condiciones, el formato es /alarma <dia/mes/año> <hora:minutos> <nombre de la alarma, solo 1 palabra> en formato de 24 horas");
         }
