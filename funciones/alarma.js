0a18ae7264b2422d327d2fcc934c893e0ffc7f2d
Daryl Alejandro Olivares Gutierrez <daryl_alejandro@hotmail.com>
2020-03-03 03:02:34 -0300

    fix

--- funciones/alarma.js
+++ funciones/alarma.js
@@ -87,22 +87,25 @@ module.exports = {
                         db.end();
                     })
 
-                    if(!message.guild.voiceConnection){
-                        message.member.voiceChannel.join().then(function(connection){
+                    if(message.guild.voiceConnection){
+                        if(message.member.voiceChannel){
+                            message.member.voiceChannel.join().then(function(connection){
                         
-                            var url = 'https://www.youtube.com/watch?v=nVCUKH1vN1g';
-                            connection.playStream(ytdl(url, {filter: "audioonly"}));
-                
-                            setTimeout(timeout, 18000);
-                            function timeout(){
-                                connection.disconnect()
-    
-                            }
-                
-                       })
-
+                                var url = 'https://www.youtube.com/watch?v=nVCUKH1vN1g';
+                                connection.playStream(ytdl(url, {filter: "audioonly"}));
+                    
+                                setTimeout(timeout, 18000);
+                                function timeout(){
+                                    connection.disconnect()
+        
+                                }
+                    
+                           })
+                        }else{
+                            console.log("No está conectado el usuario");
+                        }
                     }else{
-                        console.log("No está conectado el usuario");
+                        
                     }
                 }
     
