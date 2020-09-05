473d70adc9d5a48b4c90130455d8c8a29a74873c
Daryl Olivares <darylolivares@gmail.com>
2020-09-04 20:30:09 -0400

    imagen lenin ql

--- funciones/lenin.js
+++ funciones/lenin.js
@@ -3,13 +3,16 @@ module.exports = {
         message.channel.send('*CRITICAL HIT*');
         message.channel.send('Felicitaciones lenin culiao, te has encontrado con un lenin REculiao');
         message.channel.send("", {files: ["i.imgur.com/anJT4a1.png"]});
+        message.members.setMute(true, 'Muteo de premio, enjoy');
+        
+        
     },
     culiao: function(message){
         message.channel.send('Lenin Culiao');
     },
     AmongUs: function(message){
 
-        message.channel.send("", {files: [""]});
+        message.channel.send("", {files: ["https://i.imgur.com/NG8voV4.png"]});
 
     }
 };
